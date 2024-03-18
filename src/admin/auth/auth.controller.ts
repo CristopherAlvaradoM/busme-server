bc7455ffbb85eade5e17fc06486ff93694393080
Cristopher Yahir Alvarado Mombela <cristopher.alvarado.21s@utzmg.edu.mx>
2024-03-18 16:43:25 -0600

    Added anotations to the get admin/auth endpoint

--- src/admin/auth/auth.controller.ts
+++ src/admin/auth/auth.controller.ts
@@ -5,6 +5,14 @@ import { AuthService } from './auth.service';
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  /**
+   * Endpoint for admin authentication.
+   * 
+   * @param correo - The email address of the admin.
+   * @param contrasena - The password of the admin.
+   * @returns an encrypted token if the user is found with the admin data.
+   * @throws NotFoundException if the admin is not found.
+   */
   @Get()
    async auth(@Query('correo') correo: string, @Query('contrasena') contrasena: string){
       const auth =  await this.authService.login(correo, contrasena);
