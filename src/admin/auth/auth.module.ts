0025adbc6ed3d75600cdc3d07f143ad5191c521e
Cristopher Yahir Alvarado Mombela <cristopher.alvarado.21s@utzmg.edu.mx>
2024-03-18 11:47:15 -0600

    Modified auth.module for it to consume Usuario schema

--- src/admin/auth/auth.module.ts
+++ src/admin/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Usuario } from 'src/schemas/user.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ schema: Usuario, name: Usuario.name }])],
   controllers: [AuthController],
   providers: [AuthService]
 })
