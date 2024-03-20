ef56d2bb8b16b978f29794b43dfdd959c73f6920
Cristopher Yahir Alvarado Mombela <cristopher.alvarado.21s@utzmg.edu.mx>
2024-03-19 20:25:01 -0600

    Created endpoint admin/password-reset/token, encrypt pwd, save it, and delete the tokenResetPwd camp

--- src/admin/password_reset/password_reset.controller.ts
+++ src/admin/password_reset/password_reset.controller.ts
@@ -1,14 +1,25 @@
-import { Controller, NotFoundException, Patch, Query } from '@nestjs/common';
+import { BadRequestException, Body, Controller, NotFoundException, Patch, Query } from '@nestjs/common';
 import { PasswordResetService } from './password_reset.service';
 
 @Controller('admin/password-reset')
 export class PasswordResetController {
   constructor(private passwordResetService: PasswordResetService) {}
 
-  @Patch()
+  @Patch('password')
   async enviarToken(@Query('correo') correo: string){
-    const done = await this.passwordResetService.enviarCorreoToken(correo);
-    if(!done) throw new NotFoundException('Usuario no encontrado');
-    return done;
+    const hecho = await this.passwordResetService.enviarCorreoToken(correo);
+    if(!hecho) throw new NotFoundException('Usuario no encontrado');
+    return hecho;
+  }
+
+  @Patch('token')
+  async reestablecerPwd(@Body('token') token: string, @Body('pwd') pwd: string){
+    try {
+      return await this.passwordResetService.reestablecerPwd(token, pwd);
+    } catch (error) {
+      if(error.message === 'Usuario no encontrado') throw new NotFoundException('Usuario no encontrado');
+      if(error.message === 'Token inválido') throw new BadRequestException('Token inválido');
+      throw new BadRequestException('Faltan datos');
+    }
   }
 }
--- src/admin/password_reset/password_reset.service.ts
+++ src/admin/password_reset/password_reset.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Usuario } from 'src/schemas/user.schema';
+import * as jwt from 'jsonwebtoken'
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class PasswordResetService {
@@ -13,9 +15,8 @@ export class PasswordResetService {
   async enviarCorreoToken(correo: string): Promise<boolean> {
     const usuario = await this.usuarioModel.findOne({ correo });
     if(!usuario) return false;
-    usuario.tokenResetPwd = Math.random().toString(36).substring(2);
+    usuario.tokenResetPwd = jwt.sign({usuario: usuario._id}, process.env.JWT_SECRET_KEY)
     await usuario.save();
-
     const enviarCorreo = async () => {
       await this.mailService.sendMail({
         to: correo,
@@ -23,8 +24,21 @@ export class PasswordResetService {
         text: `Para cambiar tu contraseña, haz click en el siguiente enlace: ${this.URL}${usuario.tokenResetPwd}`
       });
     };
-
     await enviarCorreo();
     return true;
+  }
+
+  async reestablecerPwd(token: string, pwd: string): Promise<boolean> {
+    if(!token || !pwd) throw new Error('Faltan datos');
+    const { usuario: idUsuario } = jwt.verify(token, process.env.JWT_SECRET_KEY) as jwt.JwtPayload;
+    const usuario = await this.usuarioModel.findById(idUsuario);
+    if(!usuario) throw new Error('Usuario no encontrado');
+    if(!(usuario.tokenResetPwd === token)) throw new Error('Token inválido');
+
+    const salt = await bcrypt.genSalt(10);
+    const nuevaPwd = await bcrypt.hash(pwd, salt);
+    await this.usuarioModel.findByIdAndUpdate(usuario._id, { $set: {contrasena: nuevaPwd,}, $unset: {tokenResetPwd: 1}});
+
+    return true
   }
 }
