c4bc0e82ba370f21665479d3300b0e6d9ef44dff
Cristopher Yahir Alvarado Mombela <cristopher.alvarado.21s@utzmg.edu.mx>
2024-03-27 19:02:46 -0600

    moved create admin to admin controller

---
+++ src/admin/admin/admin.controller.ts
@@ -0,0 +1,20 @@
+import { BadRequestException, Body, Controller, Get, Post } from '@nestjs/common';
+import { AdminService } from './admin.service';
+import { CrearUsuarioDto } from 'src/dto/CrearUsuario.dto';
+
+@Controller('admin')
+export class AdminController {
+  constructor(private adminService: AdminService) {}
+
+  @Post()
+  async create(@Body() usuario: CrearUsuarioDto) {
+      const newUser =  this.adminService.createAdmin(usuario);
+      if(!newUser) throw new BadRequestException({mensaje: 'Peticion invalida'})
+      return newUser
+  }
+
+  @Get()
+  async finAll() {
+    
+  }
+}
