fd3c66550f0ca7c7d7567b16aff6b8432d7e222d
Cristopher Yahir Alvarado Mombela <cristopher.alvarado.21s@utzmg.edu.mx>
2024-03-27 18:05:36 -0600

    hotfix, modified authservice for it to create and login with encripted passwords

--- src/admin/auth/auth.service.ts
+++ src/admin/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { Model } from 'mongoose';
 import { Usuario } from 'src/schemas/user.schema';
 import * as jwt from 'jsonwebtoken';
 import * as CryptoJS from 'crypto-js';
+import * as bcrypt from 'bcrypt'
 import { CrearUsuarioDto } from 'src/dto/CrearUsuario.dto';
 
 @Injectable()
@@ -11,8 +12,10 @@ export class AuthService {
   constructor(@InjectModel(Usuario.name) private userModel: Model<Usuario>) {}
 
   async login(correo: string, contrasena: string): Promise<object> {
-    const usuario = await this.userModel.findOne({ correo, contrasena, tipoUsuario: "admin" });
+    const usuario = await this.userModel.findOne({ correo, tipoUsuario: "admin" });
     if (!usuario) return null
+    const auth = await bcrypt.compare(contrasena, usuario.contrasena);
+    if(!auth) return null
     const usuarioSerializado = JSON.stringify(usuario);
     const token = jwt.sign({usuario: usuarioSerializado}, process.env.JWT_SECRET_KEY);
     const tokenCifrado = CryptoJS.AES.encrypt(token, process.env.CRYPTO_SECRET_KEY).toString();
@@ -21,7 +24,10 @@ export class AuthService {
 
   async createUser(usuario: CrearUsuarioDto): Promise<Usuario> {
     const nuevoUsuario = new this.userModel(usuario);
-    return nuevoUsuario.save();
+    const salt = await bcrypt.genSalt(10)
+    const encriptedPwd = await bcrypt.hash(nuevoUsuario.contrasena, salt)
+    nuevoUsuario.contrasena = encriptedPwd
+    return await nuevoUsuario.save();
   }
 
 }
